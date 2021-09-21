76c83879107f4f1fda312c35bf3b95d5a15fdd44
Anton <anton.bartcev@gmail.com>
2021-09-22 00:37:26 +0300

    Resize the game field of window is resized, move up and down the game field

--- src/app.ts
+++ src/app.ts
@@ -30,34 +30,79 @@ DZ:
 
 */
 
+const stdin = process.stdin;
 let symbolToDraw = "#"
+let playerPos = 4;
 
-const drawGameField = (width: number, height: number) => {
-    console.clear();
+// Draw title of the game with the borders at the top of the screen
+const drawTitle = () => {
     const titleName = "F33n1k5";
     const embel = "*".repeat((width - titleName.length) / 2);
     const borders = "-".repeat(width);
     console.log(borders);
     console.log(embel + titleName + embel);
     console.log(borders);
+}
+
+// Draw game field with the player's position on it
+const drawGameField = (width: number, height: number, playerPos: number) => {
+    // Take game content to the very top of the console
+    console.clear();
+    const halfWidth = width / 2;
+    // String which contains player's icon
+    const playerPosStr = symbolToDraw.repeat(Math.ceil(halfWidth - 1))
+        + "@"
+        + symbolToDraw.repeat(halfWidth)
+    drawTitle()
+    // Draw the game field and player's icon on it, according to player's position
     for (let i = 0; i < height - 3; i++) {
-        if (i === height - 4) {
-            const halfWidth = width / 2;
-            console.log(symbolToDraw.repeat(Math.ceil(halfWidth - 1)) + "@" + symbolToDraw.repeat(halfWidth));
-        } else {
-            console.log(symbolToDraw.repeat(width));
-        }
+        i === height - playerPos
+            ? console.log(playerPosStr)
+            : console.log(symbolToDraw.repeat(width))
     }
 }
+
+
+// Reserve space for title
 const heightOffset = 3;
+let width = process.stdout.columns;
+let height = process.stdout.rows - heightOffset;
 
-const width = process.stdout.columns;
-const height = process.stdout.rows - heightOffset;
+// Scale game field if the window was resized
+process.stdout.on('resize', function () {
+    width = process.stdout.columns;
+    height = process.stdout.rows;
+    // Rerender the game field with the new field size
+    drawGameField(width, height, playerPos)
+})
+
+// Game's controls
+stdin.on('data', key => {
+    const keyStr = key.toString().toLocaleLowerCase();
+    // If "q" is pressed, exit the game
+    if (keyStr === "q") {
+        process.exit();
+    }
+    // If "w" is pressed, move player up the game field
+    if (keyStr === "w" && playerPos < height) {
+        playerPos += 1
+    }
+    // If "s" is pressed, move player down the game field
+    if (keyStr === "s" && playerPos > 4) {
+        playerPos -= 1
+    }
+    else { }
+    // Rerender the game field with the new player's position
+    drawGameField(width, height, playerPos);
+    
+})
+
+// Render the initial game field
+drawGameField(width, height, playerPos);
 
 
-drawGameField(width, height);
 
-const stdin = process.stdin;
+
 
 if (!stdin.isTTY) {
     console.error("TTY not available");
@@ -68,17 +113,8 @@ stdin.setRawMode(true);
 stdin.setEncoding('utf8');
 stdin.resume();
 
-stdin.on('data', key => {
-    const keyStr = key.toString();
-    
-    if (keyStr === "q") {
-        process.exit();
-    } else {
-        symbolToDraw = keyStr;
-        drawGameField(width, height);
-        //console.log(key.toString());
-    }
-})
+
+
 
 
 /*
